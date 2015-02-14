8b5c1ff528d1f71fbe8d494fb3ef99ccfb20060b
Franklin Wise <franklin@krave.io>
2015-02-13 19:13:25 -0800

    removed comments

--- tracing/traceMessage_test.go
+++ tracing/traceMessage_test.go
@@ -1,6 +1,9 @@
 package tracing_test
 
 import (
+	"bytes"
+	"encoding/json"
+
 	"github.com/gotgo/fw/tracing"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,17 +23,17 @@ var _ = Describe("TraceMessage", func() {
 		}
 	})
 
-	//	PContext("Binary annotations", func() {
-	//		It("should capture json bytes", func() {
-	//			td := &TraceData{
-	//				Message: "dude",
-	//			}
-	//			bts, _ := json.Marshal(td)
-	//			msg.AnnotateBinary("test", "test", bytes.NewReader(bts), "application/json")
-	//			//this is throwing
-	//			a := msg.Annotations[0].Value.(map[string]interface{})
-	//			Expect(td.Message).To(Equal(a["message"]))
-	//		})
-	//
-	//	})
+	PContext("Binary annotations", func() {
+		It("should capture json bytes", func() {
+			td := &TraceData{
+				Message: "dude",
+			}
+			bts, _ := json.Marshal(td)
+			msg.AnnotateBinary("test", "test", bytes.NewReader(bts), "application/json")
+			//this is throwing
+			a := msg.Annotations[0].Value.(map[string]interface{})
+			Expect(td.Message).To(Equal(a["message"]))
+		})
+
+	})
 })
